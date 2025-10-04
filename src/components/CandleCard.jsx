1fa7aed03b3122e3ea39e6cec201a3c85e90a7e0
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-10-04 18:59:45 +0200

    style: improve CandleCard.jsx

--- src/components/CandleCard.jsx
+++ src/components/CandleCard.jsx
@@ -1,12 +1,15 @@
 export default function CandleCard({image, category, name, scent, description, price}){
-    const container = ``
-    const section1 = ``
-    const section2 = ``
+    const container = `text-slate-700 shadow-lg shadow-slate-700/30 rounded-lg`
+    const section1 = `relative`
+    const section2 = `px-1 flex flex-col  mt-2`
+    const section3 = `flex justify-between items-center`
     const img = `h-96 w-full rounded-t-lg`
-    const categoryP = ``
-    const h2 = ``
-    const scentP = ``
-    const descP = ``
+    const categoryP = `absolute top-0 left-0 py-1 px-3
+                       bg-gradient-to-br from-purple-300 to-lime-100 rounded-br-lg 
+                       font-semibold shadow-lg text-purple-700 text-lg`
+    const h2 = `text-xl font-bold`
+    const scentP = `italic text-neutral-500 text-sm mr-1`
+    const descP = `text-sm mt-2`
     const priceP = ``
  
     return(
@@ -16,8 +19,10 @@ export default function CandleCard({image, category, name, scent, description, p
                 <p className={categoryP}>{category}</p>
             </section>
             <section className={section2}>
-                <h2 className={h2}>{name}</h2>
-                <p className={scentP}>{scent}</p>
+                <section className={section3}>
+                  <h2 className={h2}>{name}</h2>
+                  <p className={scentP}>{scent}</p>
+                </section>
                 <p className={descP}>{description}</p>
                 <p className={priceP}>£{price}</p>
             </section>
